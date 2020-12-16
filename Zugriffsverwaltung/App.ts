5eaf89141c4cf742c53d4142b34d79bb72f25d89
larsk <lars.kroll@scoop-software.de>
2020-12-16 10:11:21 +0100

    add refreshToken (which will be saved in httpOnly cookies) and a token route to get a new AT when expired

--- Zugriffsverwaltung/App.ts
+++ Zugriffsverwaltung/App.ts
@@ -1,53 +1,83 @@
 import express, { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import user from './model/user'
-import controller from "./controller/controller";
+import controller from './controller/controller';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-
-
 
 const app = express();
 const port = 3005;
-
 
 const Controller: controller = new controller();
 Controller.createUsers();
 
 const infoLogger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`A ${req.method}-request was made by ${req.ip}`);
-  next();
+    console.log(`A ${req.method}-request was made by ${req.ip}`);
+    next();
 };
 
 app.use(express.json({ limit: '1mb' }));
 
 app.use(infoLogger);
 
-app.get('/getData', Controller.authenticateJWT, (req: Request, res: Response) => {
-  res.send(req.user);
-});
+app.use(cookieParser());
+
+app.get(
+    '/getData',
+    Controller.authenticateJWT,
+    (req: Request, res: Response) => {
+        res.send(req.user);
+    },
+);
 
 app.post('/login', (req: Request, res: Response) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  // Filter user from the users array by username and password
-  const user = Controller.users.find((u) => {
-    return u.username === username && u.password === password;
-  });
+    // Filter user from the users array by username and password
+    const user = Controller.users.find((u) => {
+        return u.username === username && u.password === password;
+    });
 
-  if (user) {
-    // Generate an access token
-    const accessToken = jwt.sign(
-      { username: user.username, role: user.role },
-      process.env.ACCESS_TOKEN_SECRET!
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign(
+            { username: user.username, role: user.role },
+            process.env.ACCESS_TOKEN_SECRET!,
+            { expiresIn: '30s' },
+        );
+
+        const refreshToken = jwt.sign(
+            { username: user.username, role: user.role },
+            process.env.REFRESH_TOKEN_SECRET!,
+            { expiresIn: '24h' },
+        );
+        res.cookie('jwt', refreshToken, { httpOnly: true });
+        res.json({ accessToken });
+    } else {
+        res.send('Username or password incorrect');
+    }
+});
+
+app.post('/token', (req: Request, res: Response) => {
+    //TODO: send new AT
+    var refreshToken = req.cookies.jwt;
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET!,
+        (err: any, user: any) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            const accessToken = jwt.sign(
+                { username: user.username, role: user.role },
+                process.env.ACCESS_TOKEN_SECRET!,
+                { expiresIn: '30s' },
+            );
+            res.send({ accessToken });
+        },
     );
-
-    res.json({ accessToken });
-  } else {
-    res.send('Username or password incorrect');
-  }
 });
 
 app.listen(port, () => {
-  console.log(`Server Started at Port, ${port}`);
+    console.log(`Server Started at Port, ${port}`);
 });
--- Zugriffsverwaltung/controller/controller.ts
+++ Zugriffsverwaltung/controller/controller.ts
@@ -1,10 +1,9 @@
-import { NextFunction, Request, Response } from "express";
-import user from "../model/user";
+import { NextFunction, Request, Response } from 'express';
+import user from '../model/user';
 import jwt from 'jsonwebtoken';
 
-
 class controller {
-    users: user[]
+    users: user[];
 
     constructor() {
         this.users = this.createUsers();
@@ -23,25 +22,29 @@ class controller {
                 role: 'member',
             },
         ];
-        return this.users
+        return this.users;
     }
 
     public authenticateJWT(req: Request, res: Response, next: NextFunction) {
         const authHeader = req.headers.authorization;
         if (authHeader) {
             const token = authHeader.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err: any, user: any) => {
-                if (err) {
-                    return res.sendStatus(403);
-                }
+            jwt.verify(
+                token,
+                process.env.ACCESS_TOKEN_SECRET!,
+                (err: any, user: any) => {
+                    if (err) {
+                        return res.sendStatus(403);
+                    }
 
-                req.user = user;
-                next();
-            });
+                    req.user = user;
+                    next();
+                },
+            );
         } else {
             res.sendStatus(401);
         }
-    };
+    }
 }
 
 export default controller;
