a1dd65bd6f1e4828d493fcd28da10a7bde3a8aa6
Lars Kroll <lars.kroll@scoop-software.de>
2021-02-10 10:10:54 +0100

    add appgateway Dockerfile and docker-compose

--- AppGateway/Backend/App.js
+++ AppGateway/Backend/App.js
@@ -1,15 +1,20 @@
 import express from 'express';
-
+import { resolve } from 'path';
 const app = express();
 const port = 3000;
 
-app.use(express.static('../Frontend/build/'))
+app.use(express.static('./Frontend/build/'))
 
 app.get("/hello", (req, res) => {
     res.send("hello");
 });
 
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(resolve('./Frontend/build/index.html'));
+});
+
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Appgateway app listening at http://localhost:${port}`);
 });
